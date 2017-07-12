743d37bd7c3debd7580b617e37c1f71c5a964ef8
DoraemonYu <535185175@qq.com>
2017-07-12 10:06:41 +0800

    Add new ExpansionPack-[TransactionWrap]. Assembly decoupling. Allow assembly which is hierarchical design, conveniently to create Transaction Object, without Reference the [MicroDBHelper] DLL.

---
+++ Source/MicroDBHelperExpansionPack.TransactionWrap/AssemblyInspector.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroDBHelperExpansionPack.Internals
+{
+
+    /// <summary>
+    /// Interface
+    /// </summary>
+    internal interface IAssemblyInspectorContext
+    {
+        bool CheckAssembly();
+    }
+
+
+    /// <summary>
+    /// Main logic to check [MicroDBHelper.dll]
+    /// </summary>
+    internal class AssemblyInspectorContext : MarshalByRefObject, IAssemblyInspectorContext
+    {
+        public bool CheckAssembly()
+        {
+            //Force load DLL
+            var assembly = typeof(MicroDBHelpers.MicroDBTransaction).Assembly;
+            
+            //Check GUID
+            var guidAttribute = assembly.GetCustomAttributes(typeof(GuidAttribute), true).FirstOrDefault() as GuidAttribute;
+            if (guidAttribute == null)
+                return false;
+            if (guidAttribute.Value.Equals("1fc8371f-18f4-4693-b233-a8736f9cded7", StringComparison.OrdinalIgnoreCase) == false)
+                return false;
+
+            //Finally
+            return true;
+        }
+    }
+
+
+    /// <summary>
+    /// Inspector for checking [MicroDBHelper.dll] is can be loaded successfully
+    /// </summary>
+    internal sealed class AssemblyInspector
+    {
+        /// <summary>
+        /// Check [MicroDBHelper.dll] is can be loaded successfully
+        /// </summary>
+        /// <returns></returns>
+        public static bool CheckAssembly()
+        {
+            //Create an new temporary AppDomain
+            AppDomain appDomain = AppDomain.CreateDomain("AppDomain4AssemblyInspector", null, new AppDomainSetup
+            {
+                ShadowCopyFiles     = "true",
+                LoaderOptimization  = LoaderOptimization.MultiDomainHost,
+            });
+            
+            try
+            {
+                var assmblyLoaderType   = typeof(AssemblyInspectorContext);
+                var assemblyLoader      = (IAssemblyInspectorContext)appDomain.CreateInstanceFromAndUnwrap(assmblyLoaderType.Assembly.Location, assmblyLoaderType.FullName);
+                return assemblyLoader.CheckAssembly();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                //Release the temporary AppDomain
+                AppDomain.Unload(appDomain);
+            }
+
+        }
+    }
+}
---
+++ Source/MicroDBHelperExpansionPack.TransactionWrap/TransactionWrap.cs
@@ -0,0 +1,233 @@
+﻿using MicroDBHelpers;
+using System;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace MicroDBHelperExpansionPack
+{
+    /// <summary>
+    /// Wrap for MicroDBTransaction
+    /// </summary>
+    public class TransactionWrap
+    {
+
+        //--------Members----------
+
+        #region Transaction Object
+
+        /// <summary>
+        /// Transaction Object
+        /// </summary>
+        readonly MicroDBTransaction transaction = null;
+
+        #endregion
+
+        #region Informations
+
+        /// <summary>
+        /// Level of Isolation (read only)
+        /// </summary>
+        public IsolationLevel IsolationLevel { get { return transaction.IsolationLevel; } }
+
+        /// <summary>
+        /// the Alias Name of Connection (read only)
+        /// </summary>
+        public string ConnectionAliasName { get { return transaction.ConnectionAliasName; } }
+
+        #endregion
+
+        #region Status
+
+        /// <summary>
+        /// Mark the status for MainDLL
+        /// </summary>
+        static bool IsMainDLLLoadedCorrect = false;
+
+        #endregion
+
+
+        //--------Control----------
+
+        #region Static, Check Assembly
+
+        static TransactionWrap()
+        {
+            //Check only once
+            IsMainDLLLoadedCorrect = MicroDBHelperExpansionPack.Internals.AssemblyInspector.CheckAssembly();
+        }
+
+        static void CheckAssembly()
+        {
+            if (IsMainDLLLoadedCorrect == false)
+            {
+                const string ErrorMsg = @"Assemblies of current appDomain MUST at least one reference the [MicroDBHelper], then the [MicroDBHelperExpansionPack.TransactionWrap] will work properly.";
+
+                System.Diagnostics.Trace.WriteLine("## [MicroDBHelperExpansionPack.TransactionWrap] " + ErrorMsg);
+                throw new DllNotFoundException(ErrorMsg);
+            }
+        }
+
+        #endregion
+
+
+        #region ctor
+
+        /// <summary>
+        /// Hide default ctor
+        /// </summary>
+        private TransactionWrap(IsolationLevel IsolationLevel, string ConnectionAliasName = MicroDBHelper.ALIAS_NAME_DEFAULT)
+        {
+            transaction = MicroDBHelper.UseTransaction(IsolationLevel, ConnectionAliasName);
+        }
+
+        #endregion
+
+        #region IDisposable
+
+        /// <summary>
+        /// mark as is it disposed now
+        /// </summary>
+        public bool IsDisposed { get; private set; }
+      
+
+        /// <summary>
+        /// Dispose resources
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //Release
+                if (IsDisposed == false)
+                    transaction.Dispose();
+            }
+            
+            IsDisposed = true;
+        }
+
+        /// <summary>
+        /// destruct
+        /// </summary>
+        ~TransactionWrap()
+        {
+            Dispose(false);
+        }
+       
+        #endregion
+
+        
+        #region Implicit Conversion
+
+        /// <summary>
+        /// implicit conversion,  TransactionWrap -> MicroDBTransaction
+        /// </summary>
+        public static implicit operator MicroDBTransaction(TransactionWrap wrap)
+        {
+            if (Object.ReferenceEquals(wrap, null)) return null;
+
+            return wrap.transaction;
+        }
+
+        /// <summary>
+        /// implicit conversion,  TransactionWrap -> String
+        /// </summary>
+        public static implicit operator String(TransactionWrap wrap)
+        {
+            if (Object.ReferenceEquals(wrap, null)) return null;
+
+            return wrap.ToString();
+        }
+
+        #region Override CHECK, To avoid the "IF" check problem
+
+        /// <summary>
+        /// ==
+        /// </summary>
+        public static bool operator ==(TransactionWrap A, TransactionWrap B)
+        {
+            return Object.Equals(A, B);
+        }
+        /// <summary>
+        /// !=
+        /// </summary>
+        public static bool operator !=(TransactionWrap A, TransactionWrap B)
+        {
+            return !Object.Equals(A, B);
+        }
+        /// <summary>
+        /// Equals
+        /// </summary>
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        /// <summary>
+        /// GetHashCode
+        /// </summary>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        #endregion
+
+
+        #endregion
+
+        #region ToString()
+
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return String.Format("Transaction Object using the ConnectionAliasName:[{0}] and the IsolationLevel:[{1}] ",
+                                 transaction.ConnectionAliasName,
+                                 transaction.IsolationLevel.ToString()
+                                );
+        }
+
+        #endregion
+
+
+        #region Get New Transaction Object
+
+        /// <summary>
+        /// Get new MicroDBTransaction Object with the Transaction support<para />
+        /// ( when transaction is success，call MarkSuccess() method at last，then the transaction will automatically COMMIT; when some exception is happend then case the code NOT to call MarkSuccess() method,  the transaction will automatically Rollback )
+        /// </summary>
+        /// <param name="IsolationLevel">Level of Isolation</param>
+        /// <param name="ConnectionAliasName">the Alias Name of Connection (if not pass name,it will use the DEFAULT name instead.)</param>
+        public static TransactionWrap UseTransaction(IsolationLevel IsolationLevel, string ConnectionAliasName = MicroDBHelper.ALIAS_NAME_DEFAULT)
+        {
+            CheckAssembly();
+
+            return new TransactionWrap(IsolationLevel, ConnectionAliasName);
+        }
+
+        #endregion
+
+        #region Mark Result
+
+        /// <summary>
+        /// Mark this Transaction is Success ( before this MicroDBTransaction END,it will commit; otherwise, it wll rollback  )
+        /// </summary>
+        public void MarkSuccess()
+        {
+            if (transaction != null)
+                this.transaction.MarkSuccess();
+        }
+
+        #endregion
+    }
+
+}
